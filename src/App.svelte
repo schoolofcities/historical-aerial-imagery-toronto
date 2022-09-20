7ca8d904c02c42d9c3807f122cb41a589c79b31c
Jeff Allen <jeffallen89@gmail.com>
2022-09-20 14:17:20 -0400

    svelte app

---
+++ src/App.svelte
@@ -0,0 +1,123 @@
+<script>
+	import { onMount } from 'svelte'
+
+	import Top from "./lib/Top.svelte"
+
+	// mapboxgl.accessToken = 'pk.eyJ1Ijoic2Nob29sb2ZjaXRpZXMiLCJhIjoiY2w3aml0dHdlMHlpazNwbWh0em4xOHNlaCJ9.fXNtPGq0DqYiFvPH6p4fjQ';
+
+	// const data = Places.features;
+
+	// let map;
+	// let ctuid = "0";
+
+	// let pageWidth;
+	
+	// onMount(() => {
+	// 	map = new mapboxgl.Map({
+	// 		container: 'map', 
+	// 		style: 'mapbox://styles/schoolofcities/cl7j75gbd003b14ocub8skc7c',
+	// 		center: [-79.45, 43.65], 
+	// 		zoom: 10,
+	// 		maxZoom: 12,
+	// 		minZoom: 5,
+	// 		projection: 'globe',
+	// 		scrollZoom: true,
+	// 		attributionControl: false
+	// 	});
+		
+		
+	// 	map.addControl(new mapboxgl.AttributionControl({
+	// 		customAttribution: 'Map by <a href="http://jamaps.github.io/about.html">Jeff Allen</a>'
+	// 	}), 'bottom-right');
+	// 	map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+	// 	const scale = new mapboxgl.ScaleControl({
+	// 		maxWidth: 100,
+	// 		unit: 'metric'
+	// 		});
+	// 	map.addControl(scale, 'bottom-right');
+
+	// 	map.on('mouseenter', 'ct_fill', () => {
+	// 		map.getCanvas().style.cursor = 'pointer';
+	// 	});
+	// 	map.on('mouseleave', 'ct_fill', () => {
+	// 		map.getCanvas().style.cursor = '';
+	// 	})
+
+	// 	map.on('click', 'ct_fill', (e) => {		
+
+	// 		var features = map.queryRenderedFeatures(e.point, { layers: ['ct_fill'] });
+
+	// 		if (ctuid != features[0].properties.CTUID) {
+	// 			var style = [
+	// 				"match",
+	// 				["get", "CTUID"],
+	// 				[features[0].properties.CTUID],
+	// 				"#f1c500",
+	// 				"#fff"
+	// 			]
+	// 			map.setPaintProperty('ct_fill', 'fill-color', style)
+	// 			ctuid = features[0].properties.CTUID
+
+	// 		} else {
+	// 			map.setPaintProperty('ct_fill', 'fill-color', '#fff')
+	// 			ctuid = '0'
+	// 		}
+
+	// 	});
+
+	// });
+
+	
+
+	// function placeClick(city) {
+	// 	ctuid = "0";
+	// 	map.setPaintProperty('ct_fill', 'fill-color', '#fff');
+	// 	map.panTo(city.original.geometry.coordinates);
+	// 	map.zoonTo(10);
+	// }
+
+
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</svelte:head>
+
+
+
+
+<Top/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+	
+	
+	@font-face {
+		font-family: TradeGothicBold;
+		src: url("./assets/Trade Gothic LT Bold.ttf");
+	}
+	:root {
+		font-family: 'Roboto', sans-serif;
+	}
+
+
+	
+	
+</style>
---
+++ src/lib/Top.svelte
@@ -0,0 +1,33 @@
+<script>
+	import logo from '../assets/top-logo.svg'
+</script>
+
+<div id="bar">
+	<a href="https://www.schoolofcities.utoronto.ca/"><img src={logo} alt="School of Cities"></a>
+</div>
+
+<style>
+	#bar {
+		transform: translate3d(0,0,0);
+		position: fixed;
+   		top: 0px;
+		height: 50px;
+		background-color: #fff;
+		margin-bottom: 20px;
+		border-bottom: 1px solid rgb(219, 219, 219);
+		width: 100%;
+		min-width: 200px;
+		padding-left: 0px;
+		padding-right: 0px;
+		text-align: center;
+		z-index: 5;
+		box-shadow: 0 3px 0 0 rgba(197, 197, 197, 0.168);
+	}
+	img {
+		color: blue;
+	}
+	img:hover {
+		opacity: 0.7;
+		cursor: pointer;
+	}
+</style>
